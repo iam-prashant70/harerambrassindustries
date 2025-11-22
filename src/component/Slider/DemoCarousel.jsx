4b22ecb96a86d591b07c8969d0eaebbe83200d33
prashant <prashantmaghodiya494@gmail.com>
2025-11-22 19:39:50 +0530

    new

--- src/component/Slider/DemoCarousel.jsx
+++ src/component/Slider/DemoCarousel.jsx
@@ -17,14 +17,16 @@ const DemoCarousel = () => {
 
   const desktopImages = [
     "images/banner1.png",
-    "images/banner2.png",
-    // "images/banner3.png"
+    // "images/banner2.png",
+    "images/banner3.png"
   ];
 
   const mobileImages = [
-    "images/veticalposter.jpg",
+    // "images/veticalposter.jpg",
+    "images/veticalposter3.png",
+    "images/veticalposter4.png",
     "images/veticalposter1.jpg",
-    "images/bannerm4.jpg"
+    // "images/bannerm4.jpg"
   ];
 
   const imagesToUse = isMobile ? mobileImages : desktopImages;
